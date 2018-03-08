4eaf209ed343bf2b12d2341657b4024484740e16
John Ngundeng <john.ngundeng@ocsbstudent.ca>
2018-03-08 10:32:12 -0500

    added header comment

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: TouchAndReact
+-- Name: John Ngundeng
+-- Course: ICS2O/3C
+-- This program that displays a button. When you click the button it changes 
+-- color and displays text and an image.
 
--- Your code here
