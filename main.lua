f74d8b9fc4d5b6c214976cc0c4fa48cae8a3cd77
John Ngundeng <john.ngundeng@ocsbstudent.ca>
2018-04-11 11:02:49 -0400

    u

--- main.lua
+++ main.lua
@@ -23,51 +23,51 @@ redButton.y = display.contentHeight/2
 redButton.isVisible = false
 
 -- create text object, set it's position and make it invisible
-local textObject = display.newText (" Clicked! ", 0, 0, nil, 50)
+local textObject = display.newText ("Clicked!", 0, 0, nil, 50)
 textObject.x = display.contentWidth/2
-textObject.y = display.contentHeight/2
+textObject.y = display.contentHeight/3
 textObject:setTextColor (1, 1, 0)
-textObject.isVisible = false 
+textObject.isVisible = false
 
--- Function: BlueButtonListenter
+
+-- creating sound variable
+local Sound1 = audio.loadSound("Sounds/Clicked.mp3") 
+local Clicked
+
+-- Function: BlueButtonListener
 -- Input: touch listener
--- Output: none 
--- Description: when blue button is clicked, it will make the text appear with the
--- red button, and makw the blue button disappear
-local function BlueButtonListenter(touch)	
-	if (touch.phase == "began") then		
+-- Output: none
+-- Description: when blue button is clicked, it will make the text 
+-- appear with red button and make the blue button disappear
+local function BlueButtonListener(touch)
+	if (touch.phase == "began") then
 		blueButton.isVisible = false
-		redButton.isVisible = true 
-		textObject.isVisble = true 
-	end
-	
-	if (touch.phase == "ended") then 
-		blueButton.isVisible = true 
+		redButton.isVisible = true
+		textObject.isVisible = true
+		Clicked = audio.play(Sound1)
+	end 
+
+	if (touch.phase == "ended") then
+		blueButton.isVisible = true
 		redButton.isVisible = false
-		textObject.isVisible = false 
-	end 
-end 
-
--- Function: RedButtonListener
--- Input: touch listener 
--- Output: none 
--- Description: When the Red button is clicked, it will the text and red button 
--- disappear with the and make the blue button appear  
-local function RedButtonListener(touch)
-	if(touch.phase == "began") then 
-		blueButton.isVisible = true 
-		redbutton.ivvisible = false
-		textObject.isVisible = false 
-	end 
-
-	if (touch.phase == "ended") then 
-		blueButton.isVisible = false
-		redButton.isVisible = true 
-		textObject.isVisible = true 
+		textObject.isVisible = false
 	end
-end  
+end
+-- Function: RedButtonListener
+-- Input: touch listener
+-- Output: none
+-- Description: when red button is released, it will make the text disappear
+-- and red button
+-- disappear, and then blue button reappear
+local function RedButtonListener(touch)
 
-
+	if (touch.phase == "ended") then
+		blueButton.isVisible = true
+		redButton.isVisible = false
+		textObject.isVisible = false
+	end
+end
 -- add the event listeners 
-blueButton:addEventListener("touch",BlueButtonListenter) 
-redButton:addEventListener("touch",RedButtonListenter) 
+blueButton:addEventListener("touch",BlueButtonListener) 
+redButton:addEventListener("touch", RedButtonListener)
+
